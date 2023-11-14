075af5a95e21c6051f8219fe6fab4d629e3836d4
test <ctcuser125@ip-172-31-46-179.us-east-2.compute.internal>
2023-11-14 04:36:00 +0000

    adding java program

---
+++ svalencia11.java
@@ -0,0 +1,18 @@
+// Sandra Valencia
+// Lab 11
+// CSC-321
+
+import java.util.Scanner;
+
+public class svalencia11{
+	public static void main(String[] args) {
+		int n, s;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		n= scanner.nextInt();
+		s=n* n;
+		System.out.println("Square answer "+ n + " is: " + s + " Learn as if you will live forever, live like you will die tomorrow- Mahatma Gandhi");
+	
+	}
+}
+
